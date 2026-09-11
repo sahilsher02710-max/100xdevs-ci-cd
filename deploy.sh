28aeaeea2e9f180c4bc53008c745852a61006815
sahilsher02710-max <sahilsher02710@gmail.com>
2026-09-11 08:17:20 +0530

    repush it

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
-cd 100devs-ci-cd 
-git pull origin main 
-pnpm install 
-pnpm build 
-pnpm run start
+#!/bin/bash
+
+cd ~/100xdevs-ci-cd
+
+git pull origin main
+
+pnpm install
+
+pnpm build
+
+pm2 restart next-app || pm2 start pnpm --name "next-app" -- start
+
+pm2 save
